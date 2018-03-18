8df29f4d2dc93ebb82f0b38b5e58be157df1dd02
splasky <henrychung860326@gmail.com>
2018-03-19 01:36:00 +0800

    Compare hash function

--- calculate.c
+++ calculate.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#if defined(HASH_COMPARE)
+char* hash_txt[] = { "aphash.txt", "bkdrhash.txt", "dekhash.txt", "djbhash.txt",
+                     "elfhash.txt", "jshash.txt", "pjwhash.txt", "rshash.txt", "sdbmhash.txt"
+                   };
+#define num_of_hash_files 9
+#endif
 
 int main(void)
 {
-    FILE *fp = fopen("orig.txt", "r");
-    FILE *output = fopen("output.txt", "w");
+    FILE* fp = fopen("orig.txt", "r");
+    FILE* output = fopen("output.txt", "w");
     if (!fp) {
         printf("ERROR opening input file orig.txt\n");
         exit(0);
@@ -24,6 +32,48 @@ int main(void)
     }
     fclose(fp);
 
+#if defined(HASH_COMPARE)
+    double opt_sum_a_v[num_of_hash_files] = { 0 };
+    double opt_sum_f_v[num_of_hash_files] = { 0 };
+
+    for (int j = 0; j < num_of_hash_files; ++j) {
+
+        char file_name[50] = "hash_output/";
+        char* file = hash_txt[j];
+        strcat(file_name, file);
+
+        fp = fopen(file_name, "r");
+        if (!fp) {
+            printf("ERROR opening input file %s\n", file_name);
+            exit(0);
+        }
+        double opt_sum_a = 0.0, opt_sum_f = 0.0, opt_a, opt_f;
+        for (i = 0; i < 100; i++) {
+            if (feof(fp)) {
+                printf("ERROR: You need 100 datum instead of %d\n", i);
+                printf("run 'make run' longer to get enough information\n\n");
+                exit(0);
+            }
+            fscanf(fp, "%s %s %lf %lf\n", append, find, &opt_a, &opt_f);
+            opt_sum_a += opt_a;
+            opt_sum_f += opt_f;
+        }
+        opt_sum_a_v[j] = opt_sum_a / 100.0;
+        opt_sum_f_v[j] = opt_sum_f / 100.0;
+        fclose(fp);
+    }
+
+    fprintf(output, "append() %lf ", orig_sum_a / 100.0);
+    for (int i = 0; i < num_of_hash_files; ++i) {
+        fprintf(output, "%lf ", opt_sum_a_v[i]);
+    }
+    fprintf(output, "\n");
+
+    fprintf(output, "findName() %lf ", orig_sum_f / 100.0);
+    for (int i = 0; i < num_of_hash_files; ++i) {
+        fprintf(output, "%lf ", opt_sum_f_v[i]);
+    }
+#else
     fp = fopen("opt.txt", "r");
     if (!fp) {
         fp = fopen("orig.txt", "r");
@@ -43,9 +93,10 @@ int main(void)
         opt_sum_a += opt_a;
         opt_sum_f += opt_f;
     }
-    fprintf(output, "append() %lf %lf\n",orig_sum_a / 100.0, opt_sum_a / 100.0);
-    fprintf(output, "findName() %lf %lf", orig_sum_f / 100.0, opt_sum_f / 100.0);
-    fclose(output);
     fclose(fp);
+    fprintf(output, "append() %lf %lf\n", orig_sum_a / 100.0, opt_sum_a / 100.0);
+    fprintf(output, "findName() %lf %lf\n", orig_sum_f / 100.0, opt_sum_f / 100.0);
+#endif
+    fclose(output);
     return 0;
 }
